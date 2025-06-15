ac1e9a6f945d05ee89f848db2ad207040d571eb9
sileo18 <lucastoledo358@gmail.com>
2025-06-15 19:07:29 -0300

    feat: adding di's and json serializer converter

--- PicPay/Program.cs
+++ PicPay/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using PicPay.Infra;
+using PicPay.Mappers;
 using PicPay.Repositories;
 using PicPay.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options
@@ -13,8 +18,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options
 
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<ICarteiraRepository, CarteiraRepository>();
+builder.Services.AddScoped<ITransferenciaRepository, TransferenciaRepository>();
 
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<ICarteiraService, CarteiraService>();
+builder.Services.AddScoped<ITransferenciaService, TransferenciaService>();
+
+builder.Services.AddTransient<UsuarioMapper>();
+builder.Services.AddTransient<CarteiraMapper>();
 
 
 var app = builder.Build();
